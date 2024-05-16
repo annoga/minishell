128c42080f5d502c89bbd21737ee8bddb6c1664e
crmanzan <crmanzan@gmail.com>
2024-05-16 22:50:38 +0200

    jugando con readline

--- read_line.c
+++ read_line.c
@@ -2,47 +2,101 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 
-void readline_simple()
+void readline_simple(const char *str)
 {
 	
 	char *line;
-	line = readline("nugget: ");
+	line = readline(str);
 
 	printf("is it a nugget?: %s\n", line);
 
-	free(line);
+	//free(line);
 }
 
 void addhistory_simple_notreal()
 {
 	char *line;
-	int i;
+	int i = 0;
 	
-	for(i = 0; i < 5; i++) 
+	while(++i <= 3)
 	{
-		line = readline("Ingresa una línea de texto: ");
+		line = readline("nugget (queda guardado en el historial): ");
 		add_history(line);
 	}
+	/*
 	while(1)
 	{
-		line = readline("Ingresa una línea de texto (o presiona ctrl + c para salir): ");
-		if(line != NULL)
-		{
-			printf("La línea ingresada es: %s\n", line);
-		}
-		else 
-		{
+		line = readline("nugget (no en el historial): ");
+		if(line == NULL) 
 			break;
+	}
+	*/
+}
+
+void replace_line()
+{
+	char *line;
+	line = readline("nugget: ");
+	printf("es un puto nugget? %s\n", line);
+	rl_replace_line("this is new line: ", 0);
+	rl_redisplay();
+	line = readline("new nugget: ");
+	printf("es un nuevo nugget? %s\n", line);
+
+}
+
+void new_line()
+{
+
+	int i = 0;
+	char *line;
+
+	while(++i <= 3)
+	{
+		line = readline("nugget: ");
+		add_history(line);
+		printf("es un puto nugget? %s\n", line);
+		rl_on_new_line();
+	}
+}
+
+
+void mi_primer_minishell()
+{
+	int i = 0;
+	char *line;
+
+	while(1)
+	{
+		line = readline("nugget: ");
+		if(line[0] == 'd' && line[1] == '\0')
+		{
+			printf("you deleted the history\n");
+			rl_clear_history();
 		}
+		else
+		{
+			add_history(line);
+			printf("es un puto nugget? %s\n", line);
+		}
+		
+		rl_on_new_line();
 	}
 }
 
 
 int main()
 {
-	//readline_simple();
-	addhistory_simple_notreal();
-	
+
+//	addhistory_simple_notreal();
+//	readline_simple("nugget (sin limpiar historial): ");
+//	rl_clear_history();
+//	readline_simple("nugget (con limpiar historial): ");
+//	new_line();
+//	replace_line();
+	mi_primer_minishell();
+
+
 	return(0);
 	
 }
