daf1b60f34a62df9fb7bd72f81c5064a7f5f492e
Cristina <crmanzan@gmail.com>
2024-05-23 20:19:44 +0200

    todo lo raro comentado

--- src/main.c
+++ src/main.c
@@ -6,33 +6,37 @@
 /*   By: anovoa <anovoa@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:55:21 by angeln            #+#    #+#             */
-/*   Updated: 2024/05/23 19:35:47 by crmanzan         ###   ########.fr       */
+/*   Updated: 2024/05/23 20:16:53 by crmanzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 //#include <readline/readline.h>
 //#include <readline/history.h>
+/*
+void split_modded(char *line, char c)
+{
+	int i = 0;
+	char **split;
+	split = ft_split(line, c);
 
-
+	while(split[i])
+	{
+		printf("%d. split: %s\n", i, split[i]);
+		i++;
+	}
+}
+*/
 
 int	main(void)
 {
 	char	*line;
-	char **split;
-
 	int i = 0;
 	while (1)
 	{
 		line = readline("💩 nugget 🐾$ ");
-		split = ft_split(line, ' ');
 		i = 0;
-		while(split[i])
-		{
-			printf("%d. split: %s\n", i, split[i]);
-			i++;
-		}
-
+//		split_modded(line, '|');
 
 /*		if(line[0] == 'd' && line[1] == '\0')
 		{
