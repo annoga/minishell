041296f96acc2e6942e7c5bdc927b842dfb24578
anovoa mac arm64 <1sheep2sheep@gmail.com>
2024-05-29 16:35:37 +0200

    works when given correct parameters

--- src/echo.c
+++ src/echo.c
@@ -6,14 +6,19 @@
 /*   By: angeln <anovoa@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 20:55:00 by angeln            #+#    #+#             */
-/*   Updated: 2024/05/28 21:15:18 by angeln           ###   ########.fr       */
+/*   Updated: 2024/05/29 16:34:57 by angeln           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	echo(char **options, char *str)
+/* This function prints the given string followed by a new line. 
+ * If given the -n option, it will not add the trailing new line. */
+void	echo(char *options, char *str)
 {
-
-	printf("%s", str);
+	if (str)
+		printf("%s", str);
+	if (options)
+		if (options[0] == '-' && options[1] == 'n')
+			printf("%c", '\n');
 }
