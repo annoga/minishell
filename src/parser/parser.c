7ed7547258c6f6067d9f593f1a948ab3d2016d09
crmanzan <crmanzan@gmail.com>
2024-06-09 22:16:05 +0200

    AAAAAAAAAAAAAAAAAAAA

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,29 +1,62 @@
 #include "../../inc/minishell.h"
+void tokenize_env(t_token *tmp2, t_token *tmp, t_token *head)
+{
+    char *env;
+    env = getenv(tmp->token);
+    if(!env)
+        env = ft_strdup("");
+    else
+        env = ft_strdup(env);
+    printf("1. env token p: %p\n", env);
+    tmp = tokenizer(env);
+    add_top(tmp2, tmp);
+    if (tmp)
+        erase_one(&head, tmp2);
+    else
+    {
+        printf("4. tmp2 token p: %p\n", tmp2->token);
+
+        free(tmp2->token);
+        tmp2->token = NULL;
+        tmp2->token = env;
+    }
+    printf("4. tmp p: %p\n", tmp);
+    tmp2 = tmp;
+    printf("5. tmp2 p: %p\n", tmp2);
+    printf("6. head p: %p\n", &head);
+    tmp = tmp2;
+}
+
 t_token *prueba_env(t_token *head)
 {
     t_token *tmp;
     t_token *tmp2;
     tmp = head;
-    char *env;
+    // char *env;
     while(tmp)
     {
         if(tmp->type == ENV)
         {
+            printf("1. tmp p: %p\n", tmp);
             tmp2 = tmp;
-            env = getenv(tmp->token);
-            if(!env)
-                env = ft_strdup("");
-            tmp = tokenizer(env);
-            add_top(tmp2, tmp);
-            if (tmp)
-                erase_one(&head, tmp2);
-            else
-            {
-                free(tmp2->token);
-                tmp2->token = env;
-            }
-            tmp = tmp2;
+            printf("2. tmp2 p: %p\n", tmp2);
+            printf("3. head p: %p\n", head);
+            tokenize_env(tmp2, tmp, head);
+            // env = getenv(tmp->token);
+            // if(!env)
+            //     env = ft_strdup("");
+            // tmp = tokenizer(env);
+            // add_top(tmp2, tmp);
+            // if (tmp)
+            //     erase_one(&head, tmp2);
+            // else
+            // {
+            //     free(tmp2->token);
+            //     tmp2->token = env;
+            // }
+            // tmp = tmp2;
         }
+        printf("7. tmp->next p: %p\n", tmp->next);
         tmp = tmp->next;
     }
     tmp = head;
--- src/parser/push_midlist.c
+++ src/parser/push_midlist.c
@@ -12,6 +12,15 @@ void    add_top(t_token *head, t_token *new)
         new = new->next;
     new->next = tmp;
 }
+
+void free_single_token(t_token *token) {
+    if (token) {
+        free(token->token);
+        token->token = NULL;
+        free(token);
+    }
+}
+
 void    erase_one(t_token **head, t_token *to_erase)
 {
     t_token *aux;
@@ -22,8 +31,8 @@ void    erase_one(t_token **head, t_token *to_erase)
     if (*head == to_erase)
     {
         *head = to_erase->next;
-        to_erase->next = NULL;
-        free_token(to_erase);
+        // to_erase->next = NULL;
+        free_single_token(to_erase);
         return ;
     }
     prev = *head;
@@ -33,8 +42,9 @@ void    erase_one(t_token **head, t_token *to_erase)
         if (aux == to_erase)
         {
             prev->next = aux->next;
-            aux->next = NULL;
-            free_token(aux);
+            // aux->next = NULL;
+            free_single_token(aux);
+            aux = NULL;
             return ;
         }
         prev = aux;
