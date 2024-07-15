b5bc51fcce9b7c6991c1d7f4f93b7c6119cf4de2
Ángel Novoa <anovoa@car7s1.42barcelona.com>
2024-07-15 21:00:15 +0200

    exit works with 0 and 1 parameters

--- src/exec/ft_exit.c
+++ src/exec/ft_exit.c
@@ -6,28 +6,34 @@
 /*   By: anovoa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 13:47:15 by anovoa            #+#    #+#             */
-/*   Updated: 2024/07/14 16:35:10 by anovoa           ###   ########.fr       */
+/*   Updated: 2024/07/15 20:25:04 by anovoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+static int	is_num_error(char *token, unsigned char exit_code);
+static int	is_limit_error(int sign, char *str);
+
 void	ft_exit(t_token *head)
 {
 	unsigned char	exit_code;
-	int				err;
+
 	exit_code = 0;
-	err = 0;
 	// has args?
 	if (!head)
 		exit(0);
 	// is n1 a num?
-	if (!check_num(head->token, &err))
-		print_exit_error(err)
-		exit(2);
-	if (!ft_isdigit(atoi(head->token)))
-//	if (argc == 0)
+	exit_code = is_num_error(head->token, exit_code);
+	if (!exit_code)
+	{
+		printf("exit_code:%i", exit_code);
 		exit(exit_code);
+	}
+//		print_exit_error(err)
+	if (!ft_isdigit(atoi(head->token)))
+		exit(exit_code);
+//	if (argc == 0)
 /*	if (argc == 1)// && !isdigit(argv[0]))
 		ft_atol();
 	if (argc > 1)
@@ -37,38 +43,43 @@ void	ft_exit(t_token *head)
 	exit(exit_code);
 }
 
-static int	check_num(char *token)
+static int	is_num_error(char *token, unsigned char exit_code)
 {
-	int		i;
 	int		has_num;
 	int		sign;
 	char	*str;
+	int		i;
 
 	i = 0;
 	has_num = 0;
 	sign = 1;
 	str = ft_strtrim(token, " ");
 	if (ft_issign(str[i]))
-	{
 		if (str[i++] == '-')
-			sign = -1
-	}
-	limit_check(sign, str + i)//could have non digits
-	while (str[i] && ft_isdigit(str[i++]))
+			sign = -1;
+	exit_code = is_limit_error(sign, &str[i]);//could have non digits
+	while (str[i] && ft_isdigit(str[i]))
+	{
 		has_num = 1;
-	if (has_num && !str[i])
-		return (1);
+		i++;
+	}
+	if (exit_code != 2 && has_num && !str[i])
+		exit_code = 0;
 	else
-		return (0);
+		exit_code = 2;
+	free(str);
+	return(exit_code);
 }
 
-static int	limit_check(int sign, char *str, int *err)
+static int	is_limit_error(int sign, char *str)
 {
 	int	len;
 
+	if (!str)
+		return (2);
 	len = ft_strlen(str);
 	if (len < 19)
-		return (1);
+		return (0);
 	else if (len == 19)
 	{
 		if (sign == 1 && ft_strncmp((str), "9223372036854775807") > 0)
@@ -77,5 +88,6 @@ static int	limit_check(int sign, char *str, int *err)
 			return (2);
 	}
 	else if (len > 19)
-		return (0);
+		return (2);
+	return (0);
 }
