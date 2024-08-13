bc1b3568689b83127ea0a57b9ef6a19f9a379ce9
Cristina <crmanzan@gmail.com>
2024-08-13 20:44:59 +0200

    norminette working

--- src/parser/expansor_wildcard.c
+++ src/parser/expansor_wildcard.c
@@ -1,79 +1,89 @@
+/* ************************************************************************** */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 expansor_wildcard.c								:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: crmanzan <marvin@42.fr>					+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2024/08/13 20:32:34 by crmanzan		   #+#	  #+#			  */
+/*	 Updated: 2024/08/13 20:32:34 by crmanzan		  ###	########.fr		  */
+/*																			  */
+/* ************************************************************************** */
 #include "../../inc/minishell.h"
 
-
-static char *construct_full_path(const char *dir_path, const char *entry_name)
+static char	*construct_full_path(const char *dir_path, const char *entry_name)
 {
-    char *full_path = malloc(2048);
-    if (!full_path)
-    {
-        perror("malloc");
-        return NULL;
-    }
-    ft_strlcpy(full_path, dir_path, PATH_MAX);
-    ft_strlcat(full_path, "/", PATH_MAX);
-    ft_strlcat(full_path, (char *)entry_name, PATH_MAX);
-    return full_path;
+	char	*full_path;
+
+	full_path = malloc(2048);
+	if (!full_path)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	ft_strlcpy(full_path, dir_path, PATH_MAX);
+	ft_strlcat(full_path, "/", PATH_MAX);
+	ft_strlcat(full_path, (char *)entry_name, PATH_MAX);
+	return (full_path);
 }
 
-static t_token *allocate_token(void)
+static t_token	*allocate_token(void)
 {
-    t_token *new_token = malloc(sizeof(t_token));
-    if (!new_token)
-    {
-        perror("malloc");
-        return NULL;
-    }
-    new_token->next = NULL;
-    return new_token;
+	t_token	*new_token;
+
+	new_token = malloc(sizeof(t_token));
+	if (!new_token)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	new_token->next = NULL;
+	return (new_token);
 }
 
-static int initialize_token(t_token *token, const char *full_path)
+static int	initialize_token(t_token *token, const char *full_path)
 {
-    token->token = strdup(full_path);
-    if (!token->token)
-    {
-        perror("strdup");
-        free(token);
-        return -1;
-    }
-    token->type = ARG;
-    token->is_quote = 0;
-    return 0;
+	token->token = strdup(full_path);
+	if (!token->token)
+	{
+		perror("strdup");
+		free(token);
+		return (-1);
+	}
+	token->type = ARG;
+	token->is_quote = 0;
+	return (0);
 }
 
-t_token *create_token_from_entry(const char *dir_path, const char *entry_name)
+t_token	*create_token_from_entry(const char *dir_path, const char *entry_name)
 {
-    t_token *new_token;
-    char *full_path;
-    
-    full_path = construct_full_path(dir_path, entry_name);
-    if (!full_path)
-        return NULL;
+	t_token	*new_token;
+	char	*full_path;
 
-    new_token = allocate_token();
-    if (!new_token)
-        return (free(full_path), NULL);
-
-    if (initialize_token(new_token, full_path) < 0)
-        return (free(full_path), NULL);
-
-    return (free(full_path), new_token);
+	full_path = construct_full_path(dir_path, entry_name);
+	if (!full_path)
+		return (NULL);
+	new_token = allocate_token();
+	if (!new_token)
+		return (free(full_path), NULL);
+	if (initialize_token(new_token, full_path) < 0)
+		return (free(full_path), NULL);
+	return (free(full_path), new_token);
 }
 
-t_token *match_wildcards_in_directory(const char *dir_path, const char *pattern)
+t_token	*match_wildcards_in_directory(const char *dir_path, const char *pattern)
 {
-    DIR *dir;
-    t_token *new_tokens;
-    t_token *last_token;
+	DIR		*dir;
+	t_token	*new_tokens;
+	t_token	*last_token;
 
-    dir = open_directory(dir_path);
-    if (!dir)
-        return (NULL);
-    last_token = NULL;
-    new_tokens = NULL;
-    printf("Matching pattern %s in directory %s\n", pattern, dir_path);
-    process_directory_entries(dir, pattern, &new_tokens, &last_token);
-
-    closedir(dir);
-    return new_tokens;
+	dir = open_directory(dir_path);
+	if (!dir)
+		return (NULL);
+	last_token = NULL;
+	new_tokens = NULL;
+	printf("Matching pattern %s in directory %s\n", pattern, dir_path);
+	process_directory_entries(dir, pattern, &new_tokens, &last_token);
+	closedir(dir);
+	return (new_tokens);
 }
--- src/parser/wildcard_utils.c
+++ src/parser/wildcard_utils.c
@@ -1,47 +1,58 @@
+/* ************************************************************************** */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 wildcard_utils.c									:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: crmanzan <marvin@42.fr>					+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2024/08/13 20:42:36 by crmanzan		   #+#	  #+#			  */
+/*	 Updated: 2024/08/13 20:42:39 by crmanzan		  ###	########.fr		  */
+/*																			  */
+/* ************************************************************************** */
 #include "../../inc/minishell.h"
 
-DIR *open_directory(const char *dir_path)
+DIR	*open_directory(const char *dir_path)
 {
-    DIR *dir;
-    dir = opendir(dir_path);
-    if (!dir)
-        perror("bash: ");
-    return (dir);
+	DIR	*dir;
+	
+	dir = opendir(dir_path);
+	if (!dir)
+		perror("bash: ");
+	return (dir);
 }
-static void add_matching_token(const char *entry_name, const char *pattern, t_token **new_tokens, t_token **last_token)
+static void	add_matching_token(const char *entry_name, const char *pattern,
+				t_token **new_tokens, t_token **last_token)
 {
-    t_token *new_token;
+	t_token	*new_token;
 
-    if (ft_fnmatch(pattern, entry_name) == 0) 
-    {
-        new_token = create_token_from_entry(".", entry_name);
-        if (new_token) 
-        {
-            if (!(*new_tokens))
-                *new_tokens = new_token;
-            else
-                (*last_token)->next = new_token;
-            *last_token = new_token;
-        }
-    }
-}
-
-static int is_valid_entry(const char *entry_name)
-{
-    if(ft_strncmp(entry_name, ".") != 0 && ft_strncmp(entry_name, "..") != 0)
-        return 1;
-    return 0;
-}
-void process_directory_entries(DIR *dir, const char *pattern, t_token **new_tokens, t_token **last_token)
-{
-    struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL)
-    {
-        if (is_valid_entry(entry->d_name))
-            add_matching_token(entry->d_name, pattern, new_tokens, last_token);
-    }
+	if (ft_fnmatch(pattern, entry_name) == 0) 
+	{
+		new_token = create_token_from_entry(".", entry_name);
+		if (new_token) 
+		{
+			if (!(*new_tokens))
+				*new_tokens = new_token;
+			else
+				(*last_token)->next = new_token;
+			*last_token = new_token;
+		}
+	}
 }
 
+static int	is_valid_entry(const char *entry_name)
+{
+	if (ft_strncmp(entry_name, ".") != 0 && ft_strncmp(entry_name, "..") != 0)
+		return 1;
+	return 0;
+}
 
+void	process_directory_entries(DIR *dir, const char *pattern, t_token **new_tokens, t_token **last_token)
+{
+	struct dirent	*entry;
 
-
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (is_valid_entry(entry->d_name))
+			add_matching_token(entry->d_name, pattern, new_tokens, last_token);
+	}
+}
