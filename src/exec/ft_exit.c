a11fd05a32b966a3377e67df23124982045894d6
Ángel Novoa <anovoa@car15s1.42barcelona.com>
2024-08-15 20:06:26 +0200

    simpler exit

--- src/exec/ft_exit.c
+++ src/exec/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: anovoa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 13:47:15 by anovoa            #+#    #+#             */
-/*   Updated: 2024/07/29 22:29:34 by anovoa           ###   ########.fr       */
+/*   Updated: 2024/08/15 20:05:04 by anovoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,36 @@
 
 static int	is_num_error(char *token, unsigned char exit_code);
 static int	is_limit_error(int sign, char *str);
+
+static int	has_one_arg(t_token *head);
+static int	is_long(char *str);
+
+/* Returns 1 if we have the following sequence of tokens:
+ * ' ', 'arg1', ' ', 'arg2'
+ * Otherwise, it returns 0.
+ * */
+static int	has_one_arg(t_token *head)//32
+{
+	t_token	*arg;
+
+	arg = head->next;//arg1
+	if (arg && arg->token)
+		arg = arg->next;//32
+		if (arg && arg->token)
+			arg = arg->next;//arg2
+			if (arg && arg->token)
+				return (1);
+	return (0);
+}
+
+/* Returns 1 if the given string can be read as a number
+ * of type long.
+ * Otherwise, it returns 0.
+ * */
+static int	is_long(char *str)
+{
+	return (0);
+}
 
 void	ft_exit(t_token *head)
 {
@@ -23,12 +53,45 @@ void	ft_exit(t_token *head)
 	// has args?
 	if (!head)
 		exit(0);
+	// exit_code = read num?
+	//
+	if (has_one_arg(head))// one arg
+	{
+		if (is_long(head->next->token))// is long
+		{
+			//exit_code = (unsigned char)atol(head->next->token);
+			//exit(exit_code);
+			printf("exit_code: %s\n***\n", head->next->token);
+		}
+		else// not long
+		{
+			exit_code = 2;
+			exit(2);
+		}
+	}
+	else// multiple args
+	{
+		if (is_long(head->next->token))// arg 1 is long
+		{
+			//ToDo: Update $? to 1
+			printf("exit: too many arguments");
+			return ;
+		}
+		else// arg 1 !long
+		{
+			exit_code = 2;
+			exit(exit_code);
+		}
+	}
+//		printf("token: %s\n***\n", head->next->token);
+
+
 	// is n1 a num?
 	exit_code = is_num_error(head->token, exit_code);
-	printf("_exit_code: %i\n", exit_code);
+//	printf("_exit_code: %i\n", exit_code);
 	if (!exit_code)
 	{
-		printf("exit_code:%i", exit_code);
+//		printf("exit_code:%i", exit_code);
 		exit(exit_code);
 	}
 //		print_exit_error(err)
