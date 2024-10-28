c87281d639857dc96c643bb479f7cc63ce68b7c9
Cristina <crmanzan@gmail.com>
2024-10-28 15:18:56 +0100

    pwd hecho

--- src/exec/ft_pwd.c
+++ src/exec/ft_pwd.c
@@ -7,7 +7,6 @@ int pwd_arguments(t_cmd *cmd)
         ft_putendl_fd("pwd: minishell does not allow options", 2);
         ft_putendl_fd("pwd: usage <\"pwd\">", 2);
         return (1);
-        // cmd->status = 1;
     }
     return (0);
 }
@@ -21,7 +20,7 @@ int ft_pwd(t_cmd *cmd)
         return (free(cwd), 2);
     if (!cwd)
     {
-        perror("minishell");
+        ft_putendl_fd("minishell: pwd: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory", 2);
         exit(1);
     }
     ft_putendl_fd(cwd, 1);
