8c43a0f9ad45c5644741fa03b70b29c663934858
anovoa <1sheep2sheep@gmail.com>
2024-11-04 10:47:14 +0100

    fix unset

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: angeln <anovoa@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 01:18:04 by angeln            #+#    #+#             */
-/*   Updated: 2024/11/04 01:18:05 by angeln           ###   ########.fr       */
+/*   Updated: 2024/11/04 10:46:15 by anovoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,37 @@
 
 static void	erase_two(t_env **head, t_env *to_erase);
 static void	free_two_env(t_env *env);
+static int	unset_one(t_cmd *cmd, t_env ***env, int i);
 
 int	ft_unset(t_cmd *cmd, t_env **env)
+{
+	t_cmd	*tmp;
+	int		i;
+
+	i = 1;
+	if (!cmd || !cmd->cmd[1])
+		return (0);
+	tmp = cmd;
+	while (tmp->cmd[i])
+		unset_one(tmp, &env, i++);
+	return (0);
+}
+
+static int	unset_one(t_cmd *cmd, t_env ***env, int i)
 {
 	t_env	*tmp;
 	t_env	*next;
 
-	tmp = *env;
-	if (!cmd || !cmd->cmd[1])
+	tmp = **env;
+	if (!cmd || !cmd->cmd[i])
 		return (0);
 	while (tmp)
 	{
 		next = tmp->next;
-		if (ft_strcmp(tmp->key_name, cmd->cmd[1]) == 0)
+		if (ft_strcmp(tmp->key_name, cmd->cmd[i]) == 0)
 		{
 			erase_two(env, tmp);
-			break ;
+			return (1);
 		}
 		tmp = next;
 	}
